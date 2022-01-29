14558aaa1c359ec964ae959ce4dd1029ddfc9299
DRAGON <anantvijay3@gmail.com>
2022-01-29 13:18:13 +0530

    restore

--- app.py
+++ app.py
@@ -11,20 +11,20 @@ from solver.ocr import *
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def index():
+
     return "Works"
 
-@socketio.on('image')
+@socketio.on("image")
 def image(data_image):
+
     sbuf = io.StringIO()
     sbuf.write(data_image)
 
-    # decode and convert into image
     b = io.BytesIO(base64.b64decode(data_image))
     pimg = Image.open(b)
 
-    ## converting RGB to BGR, as opencv standards
     frame = cv2.cvtColor(np.array(pimg), cv2.COLOR_RGB2BGR)
     sudoku,image = GET_SUDOKU(frame)
     warp,points1,points2,contour = GET_PERSPECTIVE(sudoku,image)
@@ -33,18 +33,16 @@ def image(data_image):
     solved = SOLVE(unsolved)
     draw = DRAW_SOLVED(solved,warp)
     image = PUT_PERSPECTIVE(image,draw,points1,points2,contour)
-    imgencode = cv2.imencode('.jpg', image)[1]
+    imgencode = cv2.imencode(".jpg", image)[1]
 
-    # base64 encode
-    stringData = base64.b64encode(imgencode).decode('utf-8')
-    b64_src = 'data:image/jpg;base64,'
+    stringData = base64.b64encode(imgencode).decode("utf-8")
+    b64_src = "data:image/jpg;base64,"
     stringData = b64_src + stringData
 
-    # emit the frame back
-    emit('response_back', stringData)
+    emit("response_back", stringData)
     if solved:
         time.sleep(10)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     socketio.run(app)
